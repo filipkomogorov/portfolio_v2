3fa09961dcf299fea7414fac2fa897e7d61eeeab
Filip Komogorov <filipkomogorov@gmail.com>
2021-11-27 16:35:25 +0100

    frontpage completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,5 @@ const hamburgerBtn = document.querySelector("#hamburgerBtn");
 const nav = document.querySelector("nav");
 hamburgerBtn.addEventListener("click", () => {
   nav.classList.toggle("addNav");
+  hamburgerBtn.classList.toggle("btn-animation");
 });
